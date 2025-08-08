802f9d1f3fd4f2838cdb88aacd472230b60222a1
EunBin <rhdmsqls1234@naver.com>
2025-08-09 06:18:06 +0900

    [Refactor] Modify components function name

--- src/components/main/BottomNav.jsx
+++ src/components/main/BottomNav.jsx
@@ -1,7 +1,10 @@
-export default function Header() {
+import MicButton from "./MicButton";
+
+export default function BottomNav() {
   return (
     <header className="bg-yellow-100 p-4 text-xl font-bold">
       바텀네비입니다
+      <MicButton/>
     </header>
   )
 }
--- src/components/main/Intro.jsx
+++ src/components/main/Intro.jsx
@@ -1,4 +1,4 @@
-export default function Header() {
+export default function Intro() {
   return (
     <header className="bg-yellow-100 p-4 text-xl font-bold">
       인트로입니다
