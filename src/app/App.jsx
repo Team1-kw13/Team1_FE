6cfbd1d60d403aafe59d13935e731030219909f5
EunBin <rhdmsqls1234@naver.com>
2025-08-08 10:55:12 +0900

    [Chore] Initial setting

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MainPage from './main/mainpage'
+
+function App() {
+
+  return (
+    <>
+      <div className='container max-w-[430px] mx-auto bg-yellow'>
+        <MainPage/>
+      </div>
+    </>
+  )
+}
+
+export default App
