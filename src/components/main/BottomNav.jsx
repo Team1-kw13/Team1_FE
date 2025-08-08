2fb405ff7d999bd1e6a17bc69ff9b7d4f00c1767
EunBin <rhdmsqls1234@naver.com>
2025-08-09 08:20:36 +0900

    [Feat] 음성 인식 기능

--- src/components/main/BottomNav.jsx
+++ src/components/main/BottomNav.jsx
@@ -1,10 +1,13 @@
-import MicButton from "./MicButton";
+import MicButton from "./MicButton.jsx";
+import homeMenuImage from "../../assets/images/home-menu.svg";
+import userImage from "../../assets/images/user.svg";
 
 export default function BottomNav() {
   return (
-    <header className="bg-yellow-100 p-4 text-xl font-bold">
-      바텀네비입니다
-      <MicButton/>
-    </header>
+    <nav className="flex items-center justify-between p-4 bg-yellow-100">
+      <button><img src={homeMenuImage} alt="Home Menu" className="w-[32px] h-[32px]" /></button>
+      <MicButton />
+      <button><img src={userImage} alt="User Menu" className="w-[32px] h-[32px]" /></button>
+    </nav>
   )
 }
---
+++ src/components/main/MicButton.jsx
@@ -0,0 +1,56 @@
+import { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import micIcon from "../../assets/images/mic_fill.svg";
+import stopIcon from "../../assets/images/stop.svg";
+
+export default function MicButton() {
+    const [isListening, setIsListening] = useState(false);
+    const navigate = useNavigate();
+    const recognitionRef = useRef(null);
+
+    useEffect(() => {
+        if (!("webkitSpeechRecognition" in window) || "SpeechRecognition" in window) {
+            alert("음성 인식이 지원되지 않는 브라우저입니다.");
+            return;
+        } else {
+            
+            const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+            recognitionRef.current = new SpeechRecognition();
+
+            recognitionRef.current.continuous = true; //연속 인식 여부
+            recognitionRef.current.interimResults = true; //중간 결과 허용 여부
+            recognitionRef.current.lang = "ko-KR"; //한국어
+
+            recognitionRef.current.onstart = () => {
+                setIsListening(true);
+            };
+
+            recognitionRef.current.onend = () => {
+                setIsListening(false);
+                navigate("/chatroompage");
+            };
+
+            recognitionRef.current.onerror = (event) => {
+                console.error("Speech recognition error:", event.error);
+                setIsListening(false);
+            };
+        }
+    }, [navigate]);
+
+    const handleMicClick = () => {
+        if (!recognitionRef.current) return;
+
+        if (isListening) {
+            recognitionRef.current.stop();
+        } else {
+            recognitionRef.current.start();
+        }
+    };
+    
+  return (
+    <button onClick={handleMicClick} className="flex items-center bg-yellow p-4 text-[28px] font-bold rounded-[100px] border-[3px] border-white hover:border-yellow">
+      <img src={isListening ? stopIcon : micIcon} alt="Mic Icon" className="w-6 h-6" />
+      {isListening ? "인식중" : "말하기"}
+    </button>
+  )
+}
