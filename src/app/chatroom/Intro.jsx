ab93c187890faff3e7815def9c0fa7c61f16c325
Jeongseojin-d <jseojin12@naver.com>
2025-08-08 13:56:09 +0900

    [Docs] chatroom routing

---
+++ src/app/chatroom/Intro.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-yellow-100 p-4 text-xl font-bold">
+      인트로입니다
+    </header>
+  )
+}
---
+++ src/app/chatroom/ServiceButtons.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-yellow-100 p-4 text-xl font-bold">
+      서비스버튼입니다
+    </header>
+  )
+}
