b646a187e44a0a084cf5d4ebc16e5c82b62c89de
Jeongseojin-d <jseojin12@naver.com>
2025-08-11 00:45:01 +0900

    [Design] help phrase

--- src/components/main/ServiceButtons.jsx
+++ src/components/main/ServiceButtons.jsx
@@ -1,7 +1,14 @@
+import Mike from "../../assets/images/mic_fill.svg"
+
 export default function ServiceButtons() {
   return (
-    <header className="bg-yellow-100 p-4 text-xl font-bold">
-      서비스버튼입니다
-    </header>
+    <div className="bg-gray100">
+      <div className="flex gap-[4px]">
+        <img src={Mike} alt="mike" ></img>
+        <header className="font-big font-bold text-[28px]">
+          무엇을도와드릴까요?
+        </header>
+      </div>
+    </div>
   )
 }
