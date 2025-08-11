c13b3aac6f059103e1a99011095b2ed990252806
EunBin <rhdmsqls1234@naver.com>
2025-08-11 15:19:29 +0900

    [Feat] Add Lisgening Component

---
+++ src/components/chatroom/SonjuListening.jsx
@@ -0,0 +1,13 @@
+import SunImg from "../../assets/images/sun.svg"
+
+export default function SonjuListening() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full
+                    bg-[linear-gradient(to_bottom,#FFF9D6CC,_#FAFAF7)]
+                    rounded-tl-[30px] rounded-tr-[30px]">
+      <img src={SunImg} alt="손주가 듣고 있어요" className="w-[91px] h-[91px]" />
+      <div className="font-bold text-[20px]">손주가 잘 듣고 있어요!</div>
+      <div className="font-small font-light text-[24px] text-gray400">다시 말하기는 한번 더 눌러주세요!</div>
+    </div>
+  );
+}
