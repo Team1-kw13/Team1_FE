19b12a6416d1d06095d8254b62a3992b3125ba29
Jeongseojin-d <jseojin12@naver.com>
2025-08-11 13:16:19 +0900

    [Design] configure buttons

--- src/components/main/ServiceButtons.jsx
+++ src/components/main/ServiceButtons.jsx
@@ -1,14 +1,31 @@
-import Mike from "../../assets/images/mic_fill.svg"
+import {Link} from 'react-router-dom';
+import Mike from "../../assets/images/mic_fill.svg";
 
 export default function ServiceButtons() {
   return (
     <div className="bg-gray100">
-      <div className="flex gap-[4px]">
+      <div className="flex gap-[4px] mb-[19px] ml-[20px]">
         <img src={Mike} alt="mike" ></img>
         <header className="font-big font-bold text-[28px]">
           무엇을도와드릴까요?
         </header>
       </div>
+
+      <div className="flex gap-[29px] mx-[30px]">
+        {["무더위 쉼터", "동사무소", "등본 발급"].map((content,i)=>(
+          <Link 
+            to='./chatroompage' 
+            key={i} 
+            className="w-fit h-[40px] text-[22px] font-big font-bold text-gray500 
+                      bg-gray200 p-[5px] rounded-lg cursor-pointer hover:text-inherit">
+            {content}
+          </Link>
+        ))}
+      </div>
+
+      <header className='text-[24px] font-small text-gray400 font-light mt-[83px] text-center'>
+        시작하기를 누르고 말씀해주세요!
+      </header>
     </div>
   )
-}
+}/*카드일까 버튼일까*/
