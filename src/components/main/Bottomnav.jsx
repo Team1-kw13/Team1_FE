c74ab28defab9b717fcc7c7a19a7eb2c7f314988
Jeongseojin-d <jseojin12@naver.com>
2025-08-11 13:19:21 +0900

    [Design] button padding

--- src/components/main/Bottomnav.jsx
+++ src/components/main/Bottomnav.jsx
@@ -4,9 +4,9 @@ import userImage from "../../assets/images/user.svg";
 
 export default function BottomNav() {
   return (
-    <nav className="flex items-center justify-between p-4 bg-yellow-100">
+    <nav className="bg-gray100 h-[139px] flex items-center justify-between">
       <button><img src={homeMenuImage} alt="Home Menu" className="w-[32px] h-[32px]" /></button>
-      <MicButton />
+      <MicButton className="pt-[17.5px] pb-[30.5px]"></MicButton>
       <button><img src={userImage} alt="User Menu" className="w-[32px] h-[32px]" /></button>
     </nav>
   )
