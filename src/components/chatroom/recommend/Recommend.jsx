dc2874852e24d1ad8f58338f24824eda31db94ba
EunBin <rhdmsqls1234@naver.com>
2025-08-12 16:10:03 +0900

    [Refactor] Modify Recommend components

--- src/components/chatroom/recommend/Recommend.jsx
+++ src/components/chatroom/recommend/Recommend.jsx
@@ -2,7 +2,7 @@ import RecommendQ from "./RecommendQ";
 
 export default function Recommend({text}) {
   return (
-    <div className="mt-[40px] ml-[24px] mr-[24px]">
+    <div className="mt-[40px] px-4">
       <div className="font-bold text-[22px]">다음 대화는 어떠세요? </div>
       <div className="flex flex-row justify-between">
         <RecommendQ text={text} />
