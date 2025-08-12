9fde90a0280ce3972a10c5f61d921fee22dc82bd
EunBin <rhdmsqls1234@naver.com>
2025-08-12 16:13:40 +0900

    [Fix] Fix ChatRoom Page

--- src/components/chatroom/chatroom.jsx
+++ src/components/chatroom/chatroom.jsx
@@ -1,4 +1,5 @@
 import Call from "./call/CallGuide";
+import ChatSummary from "./chat_summary";
 import Place from "./place/PlaceGuide";
 import Recommend from "./recommend/Recommend";
 import SonjuBubble from "./SonjuBubble";
@@ -7,22 +8,23 @@ import UserBubble from "./UserBubble";
 
 export default function ChatRoom() {
   return (
-    <div className="flex flex-col rounded-tl-[30px] rounded-tr-[30px] w-full fixed top-[90px] pt-[25px] relative"
-      style={{ boxShadow: "0 4px 10px 0px rgba(0, 0, 0, 0.15)" }}>
-      <div className="flex items-center justify-center">
-        <div className="font-small font-light text-[13px] text-gray-400 mb-[27px]">AI가 생성한 응답입니다. 중요한 정보는 꼭 확인해주세요.</div>
+    <div className="flex flex-col rounded-tl-[30px] rounded-tr-[30px] w-full h-full relative z-30"
+         style={{ boxShadow: "0 4px 10px 0px rgba(0, 0, 0, 0.15)" }}>
+      <div className="flex-shrink-0 flex items-center justify-center pt-[25px]">
+        <div className="font-small font-light text-[13px] text-gray-400 pb-[27px]">AI가 생성한 응답입니다. 중요한 정보는 꼭 확인해주세요.</div>
       </div>
-      <div className="overflow-y-auto pb-[300px]">
-      <UserBubble text="우왕 성공이다. 이거 컴포넌트 설정해놔서 알아서 늘어났다가 줄어들었다가 함 루룰" />
-      <UserBubble text="등본 떼줘" />
-      <UserBubble text="..." />
-      <SonjuBubble/>
-      <Recommend text="가까운 동사무소 알려줘" />
-      <SonjuListening />
-      <Place communityCenter="중계1동 주민센터" />
-      <Call communityCenter="중계1동 주민센터" number="02-131-2340" />        
+      <div className="flex-1 overflow-y-auto pb-[90px]">
+        <UserBubble text="우왕 성공이다. 이거 컴포넌트 설정해놔서 알아서 늘어났다가 줄어들었다가 함 루룰" />
+        <UserBubble text="등본 떼줘" />
+        <UserBubble text="..." />
+        <UserBubble text="컴포넌트 제작 중임다~" />
+        <SonjuBubble/>
+        <Recommend text="영업 시간 알려줘" />
+        <SonjuListening />
+        <Place communityCenter="중계1동 주민센터" />
+        <Call communityCenter="중계1동 주민센터" number="02-131-2340" />
+        <ChatSummary />
       </div>
-
     </div>
   );
 }
