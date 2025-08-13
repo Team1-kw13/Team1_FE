d8a0fde1efc8fd9547ced9e4d6b287181de658a5
EunBin <rhdmsqls1234@naver.com>
2025-08-13 14:37:28 +0900

    [Design] Modify Recommend components

--- src/components/chatroom/recommend/Recommend.jsx
+++ src/components/chatroom/recommend/Recommend.jsx
@@ -2,8 +2,8 @@ import RecommendQ from "./RecommendQ";
 
 export default function Recommend({text}) {
   return (
-    <div className="mt-[40px] px-4">
-      <div className="font-bold text-[22px]">다음 대화는 어떠세요? </div>
+    <div className="mt-[40px] px-6">
+      <div className="font-bold text-[#000000] text-[22px]">다음 대화는 어떠세요? </div>
       <div className="flex flex-row justify-between">
         <RecommendQ text={text} />
         <RecommendQ text={text} />
--- src/components/chatroom/recommend/RecommendQ.jsx
+++ src/components/chatroom/recommend/RecommendQ.jsx
@@ -1,7 +1,8 @@
 export default function RecommendQ({ text }) {
   return (
-      <div className="mt-[32px] flex justify-center items-center font-bold text-[22px] 
-                      w-[176px] h-[103px] rounded-[10px] gap-[10px] bg-gray200 p-[10px] cursor-pointer"> {/* 수정 필요*/}
+      <div className="mt-[32px] p-3 flex flex-col justify-center items-center font-bold text-[22px] text-gray500
+                      w-[176px] h-[103px] rounded-[10px] gap-[10px] bg-gray200 cursor-pointer"
+                      style={{wordBreak:'keep-all'}}> {/* 수정 필요*/}
         {text}
       </div>
   );
