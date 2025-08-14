5e1ff40aea7dda3ad4563d69d8cbf7cd3fb7a37a
EunBin <rhdmsqls1234@naver.com>
2025-08-14 15:15:09 +0900

    [Design] Modify ChatRoom page

--- src/app/chatroom/page.jsx
+++ src/app/chatroom/page.jsx
@@ -4,13 +4,13 @@ import Header from "../../components/main/Header";
 
 export default function ChatRoomPage() {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen w-full max-w-[430px]">
       <div className="fixed top-0 w-full max-w-[430px] z-20">
         <Header />
       </div>
       
       <div className="flex-1 overflow-hidden relative z-30
-                      mt-[89.37px] mb-[130px] w-full"
+                      mt-[89.37px] mb-[130px] w-full min-w-[430px]"
            style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
         <style>
           {`
