ce61303bf7fea913ebb2af979ecfac935d16ba84
Jeongseojin-d <jseojin12@naver.com>
2025-08-16 00:06:39 +0900

    [Test] Transition over time

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -5,27 +5,16 @@ import ServicingSun from "../../components/main/ServicingSun"
 import ServiceButtons from "../../components/main/ServiceButtons"
 import Servicing from "../../components/main/Servicing"
 import BottomNav from "../../components/main/BottomNav"
+import MicButton from "../../components/main/MicButton"
 
 export default function Page() {
-  const [listeningStatus, setListeningStatus] = useState("idle");
-  useEffect(() => {
-    setTimeout(() => setListeningStatus("listening"), 2000);
-    setTimeout(() => setListeningStatus("complete"), 4000);
-  }, []);
-  /*테스트용 흐름. 음성인식 API이벤트랑 연동해야함*/
+  const [isMicClicked,setIsMicClicked]=useState(false);
 
   return (
     <div className="flex flex-col justify-between max-h-screen w-[430px]">
       <Header />
-
-      {listeningStatus === "idle" && <Intro />}
-      {listeningStatus === "listening" && <ServicingSun />}
-      {listeningStatus === "complete" && <ServicingSun />}
-
-      {listeningStatus === "idle" && <ServiceButtons />}
-      {listeningStatus === "listening" && <Servicing />}
-      {listeningStatus === "complete" && <ServiceComplete />}
-
+      {isMicClicked?<ServicingSun />:<Intro/>}
+      {isMicClicked?<Servicing/>:<ServiceButtons />}
       <BottomNav />
     </div>
   )
--- src/components/main/Servicing.jsx
+++ src/components/main/Servicing.jsx
@@ -6,26 +6,31 @@ import Wave3 from "../../assets/images/Voice_Wave3.svg"
 import Wave4 from "../../assets/images/Voice_Wave4.svg"
 import Wave5 from "../../assets/images/Voice_Wave5.svg"
 
-const waves=[Wave1.Wave2.Wave3,Wave4,Wave5];
+const waves=[Wave1,Wave2,Wave3,Wave4,Wave5];
 
-export default function Servicing({isComplete,recognizedText}){
-    const [waveIndex, setWaveIndex] = useState(0);
+export default function Servicing({isListening}){
+    const [waveIndex,setWaveIndex]=useState(0);
 
-    useEffect(() => {
-        if (!isComplete) {
-        const interval = setInterval(() => {
-            setWaveIndex(Math.floor(Math.random() * waves.length));
-        }, 300); // 0.3초마다 랜덤 변경
-        return () => clearInterval(interval);
+    useEffect(()=>{
+        let interval;
+        if(isListening){
+            interval=setInterval(()=>{
+                setWaveIndex(Math.floor(Math.random()*waves.length));
+            },300);
         }
-    }, [isComplete]);
+        return()=>clearInterval(interval);
+    },[isListening]);
 
     return (
-        <div className="flex flex-col gap-[36px] items-center bg-gray100">
+        <div className="flex flex-col items-center bg-gray100">
         {/* 인식된 텍스트 */}
-        <div className="text-[28px] font-big font-bold mt-[18px]">
-            {recognizedText || "..." }
-        </div>
+        {isListening ? (
+            <img src={listening} className="w-[45px] h-[11px] mt-[36px]" />
+        ) : (
+            <div className="text-[28px] font-bold font-big mt-[18px]">
+            {recognizedText}
+            </div>
+        )}
 
         <img src={waves[waveIndex]} alt="voice wave" />
         
