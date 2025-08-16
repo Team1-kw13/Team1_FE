f23e06922950929d9e79dbf41e06a8566a59aa7a
EunBin <rhdmsqls1234@naver.com>
2025-08-16 10:45:15 +0900

    [Feat] 뒤로 가기 방지 및 모달 연결

--- src/components/chatroom/chat_summary.jsx
+++ src/components/chatroom/chat_summary.jsx
@@ -1,12 +1,18 @@
 import { useNavigate } from "react-router-dom";
+import webSocketService from "../../service/websocketService";
 
 export default function ChatSummary() {
   const navigate = useNavigate();
 
+  const handleSummaryClick = () => {
+    webSocketService.requestSummary();
+    navigate("/summary", {replace: true});
+  };
+
   return (
     <div className="flex flex-col items-center gap-[27px]">
       <div className="font-bold text-[20px] text-[#000000]">지금까지 주고받은 민원 내용을 정리해드릴까요?</div>
-      <button onClick={() => navigate("/summary")} className="w-[176px] h-[66px] bg-gray200 font-bold text-[22px] px-[46px] py-[16.5px] rounded-[10px] gap-[10px]">민원 요약</button>
+      <button onClick={handleSummaryClick} className="w-[176px] h-[66px] bg-gray200 font-bold text-[22px] px-[46px] py-[16.5px] rounded-[10px] gap-[10px]">민원 요약</button>
     </div>
   );
 }
--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import hamburgerMenuImg from "../../assets/images/hamburger-menu.svg";
 import headersvg from "../../assets/images/header.svg";
+import ServicePreparingModal from "../_common/ServicePreparing";
 
 export default function Header() {
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleMenuClick = () => {
-    navigate("/");
+    navigate("/", {replace: true });
   };//<손주ai> 누르면 홈화면 이동
+
+  const handleHamburgerClick = () => {
+    setIsModalOpen(true);
+  };//햄버거 메뉴 클릭 시 모달 열기
 
   return (
     <div className="flex justify-between items-center px-[24px]"
@@ -15,9 +22,10 @@ export default function Header() {
       <header onClick={handleMenuClick} className="text-[33px] font-logo cursor-pointer text-[#302200]">
         손주AI
       </header>
-      <button className="bg-transparent border-none">
+      <button onClick={handleHamburgerClick} className="bg-transparent border-none">
         <img src={hamburgerMenuImg} alt="Menu" />
       </button>
+      <ServicePreparingModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
   )
 }
