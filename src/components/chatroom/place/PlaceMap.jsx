c5583b3730515dc5528a4e932a85bacdf9ca4d71
Jeongseojin-d <jseojin12@naver.com>
2025-08-17 02:18:03 +0900

    [Test] map

--- src/components/chatroom/place/PlaceMap.jsx
+++ src/components/chatroom/place/PlaceMap.jsx
@@ -1,5 +1,38 @@
-export default function PlaceMap() {
+import { useEffect,useRef } from "react";
+
+export default function PlaceMap({officeInfo}) {
+    if(!officeInfo) return null; //데이터 없으면 렌더링 안함
+    const {tel,pos}=officeInfo;
+
+    {/*api연결시
+  const {mapRef}=useRef(null);
+
+    useEffect(()=>{
+        if(!officeInfo) return; //데이터 없으면 렌더링 안함
+
+        const {pos}=officeInfo;//위도,경도
+
+        //카카오맵 생성
+        const map=new window.kakao.maps.Map(mapRef.current,{
+            center:new window.kakao.maps.LatLng(pos[0],pos[1]),
+            level:3,
+        });
+
+        //마커 생성
+        new window.kakao.maps.Marker({
+            map,
+            position:new window.kakaomaps.LatLng(pos[0],pos[1]),
+        });
+    },[officeInfo]);
+    */}
+
     return (
-            <div className="w-[124px] h-[124px] rounded-[20px] border-[1px] border-black cursor-pointer">지도 컴포넌트</div>
+            <div 
+                //ref={mapRef}
+                className="w-[124px] h-[124px] rounded-[20px] border-[1px] border-black cursor-pointer">
+                <div>{tel}</div>
+                <div>{`위도:${pos[0]}`}</div>
+                <div>{`경도:${pos[1]}`}</div>
+            </div>
     );
 }
