e22faddba441344591d664283e02bc05e1d9c900
EunBin <rhdmsqls1234@naver.com>
2025-08-16 10:53:33 +0900

    [Style] Servicing 오타 수정

--- src/components/main/Servicing.jsx
+++ src/components/main/Servicing.jsx
@@ -1,12 +1,11 @@
 import {useState,useEffect} from "react"
-import listening from "../../assets/images/listening.svg"
 import Wave1 from "../../assets/images/Voice_Wave1.svg"
 import Wave2 from "../../assets/images/Voice_Wave2.svg"
 import Wave3 from "../../assets/images/Voice_Wave3.svg"
 import Wave4 from "../../assets/images/Voice_Wave4.svg"
 import Wave5 from "../../assets/images/Voice_Wave5.svg"
 
-const waves=[Wave1.Wave2.Wave3,Wave4,Wave5];
+const waves=[Wave1,Wave2,Wave3,Wave4,Wave5];
 
 export default function Servicing({isComplete,recognizedText}){
     const [waveIndex, setWaveIndex] = useState(0);
@@ -22,16 +21,16 @@ export default function Servicing({isComplete,recognizedText}){
 
     return (
         <div className="flex flex-col gap-[36px] items-center bg-gray100">
-        {/* 인식된 텍스트 */}
-        <div className="text-[28px] font-big font-bold mt-[18px]">
-            {recognizedText || "..." }
-        </div>
+            {/* 인식된 텍스트 */}
+            <div className="text-[28px] font-big font-bold mt-[18px]">
+                {recognizedText || "..." }
+            </div>
 
-        <img src={waves[waveIndex]} alt="voice wave" />
-        
-        <div className="font-small font-light text-[17px] text-gray400 mb-[32px]">
-            {isComplete ? "인식 완료했습니다" : "인식 중 입니다"}
-        </div>
+            <img src={waves[waveIndex]} alt="voice wave" />
+            
+            <div className="font-small font-light text-[17px] text-gray400 mb-[32px]">
+                {isComplete ? "인식 완료! 채팅창에서 안내할게요!" : "인식 중 입니다"}
+            </div>
         </div>
     );
 }
