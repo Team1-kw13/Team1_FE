5aef99029c4560b742f4112161fec137ce94c27d
EunBin <rhdmsqls1234@naver.com>
2025-08-18 12:07:03 +0900

    [Refactor] Chat, Summary page 구조 개선

--- src/app/summary/page.jsx
+++ src/app/summary/page.jsx
@@ -4,14 +4,11 @@ import Header from "../../components/main/Header";
 
 export default function SummaryPage() {
   return (
-    <div className="flex flex-col h-screen w-[430px]">
-      <div className="fixed top-0 w-full max-w-[430px]">
-        <Header />
-      </div>
-      
-      <div className="flex flex-1 overflow-hidden relative z-30
-                      mt-[89.37px] mb-[130px] w-full max-w-[430px]"
-           style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}>
+    <div className="flex flex-col h-screen w-full max-w-[430px]">
+      <Header />
+
+      <div className="flex-1 overflow-hidden w-full min-w-[430px]"
+           style={{ scrollbarWidth: "none", msOverflowStyle: "none", marginTop: "-30px" }}>
         <style>
           {`
             ::-webkit-scrollbar {
@@ -22,9 +19,7 @@ export default function SummaryPage() {
         <Summary />
       </div>
 
-      <div className="fixed bottom-0 w-full max-w-[430px] bottom-nav">
-        <BottomNav />
-      </div>
+      <BottomNav />
     </div>
   )
 }
