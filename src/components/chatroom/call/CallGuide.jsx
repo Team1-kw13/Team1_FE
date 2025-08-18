cefa0098838db142f9e5e649e223d76eaba75840
EunBin <rhdmsqls1234@naver.com>
2025-08-18 13:27:21 +0900

    [Design] Modify Call margin

--- src/components/chatroom/call/CallGuide.jsx
+++ src/components/chatroom/call/CallGuide.jsx
@@ -5,7 +5,7 @@ export default function Call({ communityCenter, number = "02-1234-5678" }) {
     }
 
     return (
-        <div className="w-full h-full flex flex-col px-6">
+        <div className="w-full flex flex-col px-6 pb-[12px]">
             <div className="text-[24px] text-[#222222] font-bold">{communityCenter} 전화번호는<br/>{number} 입니다.<br/>전화 연결을 원하시면 아래 버튼을 눌러<br/>주세요.</div>
             <div className="flex flex-col items-center">
                 <button
